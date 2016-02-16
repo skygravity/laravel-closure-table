863da33aa58ba20ca9ae8f2c926f960580d93dcf
skygravity <skygravity8@gmail.com>
2016-02-16 16:18:50 +0200

    Create create_comments_table.php

---
+++ Migrations/create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('comment_id');
+            $table->integer('bug_id')->unsigned();
+            $table->foreign('bug_id')->references('id')->on('bugs');
+            $table->integer('author')->unsigned();
+            $table->foreign('author')->references('id')->on('users');
+            $table->text('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comments');
+    }
+}
