680b03feff52e5d6eb5883602e09c34174fd4b0e
skygravity <skygravity8@gmail.com>
2016-02-16 16:19:36 +0200

    Create create_tags_table.php

---
+++ Migrations/create_tags_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->integer('bug_id')->unsigned();
+            $table->foreign('bug_id')->references('id')->on('bugs');
+            $table->string('tag', 20);
+            $table->primary(['bug_id','tag']);            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tags');
+    }
+}
