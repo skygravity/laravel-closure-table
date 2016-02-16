671b3682c30b593e6d44f9438de5c9ac3d765d78
skygravity <skygravity8@gmail.com>
2016-02-16 15:58:36 +0200

    Create create_users_table.php

---
+++ Migrations/create_users_table.php
@@ -0,0 +1,38 @@
+## This will create migration file for users
+php artisan make:migration create_users_table --create=users
+```
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('password', 60);
+            $table->rememberToken();
+            $table->decimal('hourly_rate', 9, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
+```
